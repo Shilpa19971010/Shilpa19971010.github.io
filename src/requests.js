b488ef2664bda9b4b3921ae74823b12c2fb96b86
Shilpa19971010 <66195410+Shilpa19971010@users.noreply.github.com>
2024-01-12 01:27:40 +0530

    all components

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "faf3e687328ea816b2144836aca8e14e";
+//2384a4080ba484d85d21282ebae18268
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&language=en-US&sort_by=release_date.asc&networks=Netflix`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
